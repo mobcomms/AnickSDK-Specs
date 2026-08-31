6f405efafa855c3195d8c9faffe158c2e522e612
KyeongHun Kim <khkim@anick.io>
2026-08-31 19:36:10 +0900

    Revert "fix: Pangle SPM 버전을 7.9.1-release.1로 변경"
    This reverts commit 4a7ac3c4f81c54383ff104ced0dd3541215bb7ff.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/bytedance/AdsGlobalPackage", exact: "7.9.1-release.1"),
+        .package(url: "https://github.com/bytedance/AdsGlobalPackage", exact: "7.9.0-release.8"),
     ],
     targets: [
         .binaryTarget(
