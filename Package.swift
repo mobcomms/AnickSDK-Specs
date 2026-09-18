72d74e47b281817ccebf94ef3d766c0b35ec8f30
최수훈 <shchoi2@anick.io>
2026-09-18 13:25:16 +0900

    release: SPM 2.0.13 (base + happypoint/Pangle 8.1.0-release.9)

--- Package.swift
+++ Package.swift
@@ -18,18 +18,18 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/bytedance/AdsGlobalPackage", exact: "7.9.1-release.3"),
+        .package(url: "https://github.com/bytedance/AdsGlobalPackage", exact: "8.1.0-release.9"),
     ],
     targets: [
         .binaryTarget(
             name: "AnickSDKFramework",
-            url: "https://github.com/mobcomms/AnickSDK-Specs/releases/download/2.0.12/AnickSDKFramework.xcframework.zip",
-            checksum: "7c2d1f89cd699a58b6d05f4f5e15e82f3cdf620c85708e14471bb7783b31e92c"
+            url: "https://github.com/mobcomms/AnickSDK-Specs/releases/download/2.0.13/AnickSDKFramework.xcframework.zip",
+            checksum: "7dabda693b5dd6f117efddc81aff7fab8c7ea56c6307fc9313745fbb2bad9075"
         ),
         .binaryTarget(
             name: "AnickSDKFrameworkBinary_Happypoint",
-            url: "https://github.com/mobcomms/AnickSDK-Specs/releases/download/2.0.12/AnickSDKFramework_happypoint.xcframework.zip",
-            checksum: "f488b38274ebdd29cba2d085e19e1f4720132875d46064be9a00b12b5edf6121"
+            url: "https://github.com/mobcomms/AnickSDK-Specs/releases/download/2.0.13/AnickSDKFramework_happypoint.xcframework.zip",
+            checksum: "741f186b632478dab856db53f0816f4e728afffc66072a99da05236cfcddd19a"
         ),
         .target(
             name: "AnickSDKFramework_Happypoint",
